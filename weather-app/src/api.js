9e3c0ec20a47cef08fa64a21d17fbf6401bceca1
Christy Crook <ccrook3@gmail.com>
2022-03-28 15:35:46 -0500

    got first api fetch running

---
+++ weather-app/src/api.js
@@ -0,0 +1,2 @@
+export const API_KEY = ['58ce4826be9e3265da52c88e1aaf9fae'];
+export const API_BASE_URL = 'http://api.openweathermap.org/';
--- weather-app/src/components/CitySearch.js
+++ weather-app/src/components/CitySearch.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import { API_KEY, API_BASE_URL } from '../api';
 
 const CitySearch = () => {
-    
+    const [city, setCity] = useState('');
 
-    return(
-        <form>
-            <h1>Search Your City</h1>
-            <input 
-                type="text"
-                placeholder='Search Your City'
-            >
-            </input>
-            <br></br>
-            <br></br>
-            <Button variant="contained">Check Weather</Button>
-        </form>
-    )
+    const onSearch = () => {
+        fetch(`${ API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
+            .then((response) => response.json())
+            .then((result) => console.log(result));
+    }
+
+    
+        return(
+            <form>
+                <h1>Search Your City</h1>
+                <input 
+                    type="text"
+                    placeholder='Search Your City'
+                    onChange={(event) => setCity(event.target.value)}
+                    value={city}
+                >
+                </input>
+                <br></br>
+                <br></br>
+                <Button 
+                    variant="contained" 
+                    onClick={onSearch}
+                >Check Weather</Button>
+            </form>
+        )
+    
 };
 
 export default CitySearch;
