e3dba4b82e74c16e441a5f6c03d33a3d41023e6c
Christy Crook <ccrook3@gmail.com>
2022-03-31 18:46:49 -0500

    added a clickable feature

--- weather-app/src/components/WeatherCard.js
+++ weather-app/src/components/WeatherCard.js
@@ -5,37 +5,42 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import { CardActionArea } from "@mui/material";
 
 const WeatherCard = ({dt, main, temp_max, temp_min, icon}) => {
     const date = new Date(dt);
     return (
-        <Container fixed>
+        <Container >
             <Grid
                 container spacing={2}
                 direction="row"
+                justifyContent="center"
             >
                 <Card 
                     sx={{ maxWidth: 225}}
                     variant="outlined"
                 >
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}></CardMedia>
-                    <CardContent>
-                        <Typography>
-                            {main}
-                        </Typography>
-                        <Typography>
-                            {date.toLocaleDateString()} - {date.toLocaleTimeString()}
-                        </Typography>
-                        <Typography>
-                           Min: {temp_min}
-                        </Typography>
-                        <Typography>
-                            Max: {temp_max}
-                        </Typography>
-                    </CardContent>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            src={`http://openweathermap.org/img/wn/${icon}@2x.png`}>
+                        </CardMedia>
+                        <CardContent>
+                            <Typography>
+                                {main}
+                            </Typography>
+                            <Typography>
+                                {date.toLocaleDateString()} - {date.toLocaleTimeString()}
+                            </Typography>
+                            <Typography>
+                               Min: {temp_min}
+                            </Typography>
+                            <Typography>
+                                Max: {temp_max}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
                 </Card>
             </Grid>
         </Container>
